13e45118ad796c9a5085d9f25bb63d5286097623
fanxianyu <1137558773@qq.com>
2020-02-22 13:35:14 +0800

    项目起步 添加login

---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+// 提取出路由
+
+// 1下载路由
+
+// 2导入路由
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+// 3注册路由
+Vue.use(VueRouter)
+
+// 4准备组件
+import login from '../views/login/index.vue'
+
+// 5设置路由规则
+const routes=[
+    {path:'/login',component:login}
+]
+
+// 6创建路由对象
+const router = new VueRouter({
+    routes
+})
+
+export default router
